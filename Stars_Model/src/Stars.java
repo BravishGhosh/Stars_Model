e05daf0679efe9708124fb0ac0c50dbfdab8eec0
LoopGlitch_ <grandson.baba.2012@gmail.com>
2020-03-28 10:37:31 +0530

    Application Stars for printing asterisks aesthetic
    Very basic, printing program, for trial basis

---
+++ Stars_Model/src/Stars.java
@@ -0,0 +1,20 @@
+
+public class Stars {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+final String m1="***********";
+final String m2=" ********* ";
+final String m3="  *******  ";
+final String m4="   *****   ";
+final String m5="    ***    ";
+final String m6="     *     ";
+System.out.println(m1);
+System.out.println(m2);
+System.out.println(m3);
+System.out.println(m4);
+System.out.println(m5);
+System.out.println(m6);
+	}
+
+}
